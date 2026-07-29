02bad247d6148a56ed1d0fab8e07e491d4ec44b2
pongrat555-code <pongrat555@gmail.com>
2026-07-30 00:18:31 +0700

    Update rhythm1.py

--- rhythm1.py
+++ rhythm1.py
@@ -129,12 +129,12 @@ vibration_code = """
             activeTimeouts.push(t);
         }
 
-        // เมื่อจบครบรอบ 10 ครั้ง ให้เริ่มรอบใหม่ (บวกเว้นระยะท้ายลูปเล็กน้อย 150ms)
+        // เมื่อจบครบรอบ 10 ครั้ง ให้เริ่มรอบใหม่ (บวกเว้นระยะท้ายลูปเล็กน้อย 200ms)
         loopTimeout = setTimeout(() => {
             if (isPlaying) {
                 playRhythmSequence();
             }
-        }, cumulativeTime + 150);
+        }, cumulativeTime + 200);
         activeTimeouts.push(loopTimeout);
     }
 
